82d9dfb8554c05a1f7aa83aa5cd7bfa36862d1ca
Eric Yager <ejyager00@protonmail.com>
2024-11-20 21:25:54 -0500

    Stub install command

--- cmd/install.go
+++ cmd/install.go
@@ -2,3 +2,28 @@
 Copyright © 2024 Eric Yager
 */
 package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var installCmd = &cobra.Command{
+	Use:   "install [file]",
+	Short: "Install the tools and extensions specified in the configuration file.",
+	Long:  `Install the tools and extensions specified in the configuration file.`,
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		schema, err := parseYamlFile(args[0])
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		for _, tool := range schema.Tools {
+			fmt.Println(tool.Name)
+		}
+	},
+}
